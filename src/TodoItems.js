f4ae1cc8751368daf6dafbb1f179a5c1b48aeaf8
Keren Rogel <kerenrogel@gmail.com>
2019-05-13 13:28:10 +0800

    Todolist commit

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,23 +1,26 @@
 import React,  { Component } from "react";
 import FlipMove from "react-flip-move";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 class TodoItems extends Component {
 	constructor(props) {
 		super(props);
-
 		this.createTasks = this.createTasks.bind(this);
 	}
 
 	createTasks(item) {
-		return <li onClick={() => this.delete(item.key)}
+		return <li onClick={() => this.delete(item.key, item.text)}
 								key={item.key}>{item.text}</li>		
 	}
 
-	delete(key) {
-		console.log("Key value: " + key);
+	delete(key, text) {
 		this.props.delete(key);
+		toast("Deleted: " + text, {
+			hideProgressBar: false
+		});
 	}
-
+	r
 	render() {
 		var todoEntries = this.props.entries;
 		var listItems = todoEntries.map(this.createTasks);
@@ -27,6 +30,7 @@ class TodoItems extends Component {
 				<FlipMove duration={150} easing="ease-out">
 					{listItems}
 				</FlipMove>
+				<ToastContainer hideProgressBar={false} />
 			</ul>
 		);
 	}
